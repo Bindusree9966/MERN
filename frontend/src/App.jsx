f793a2dd80ce1371c226449548a9a38a80848720
Bindusree9966 <bindusree9966@gmail.com>
2026-03-03 14:39:45 +0530

    react

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,26 @@
-import React from 'react';
-import User from './components/User';
-import Parent from './components/Parent';
-import State from './hooks/State';
-import Form from './hooks/Form';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import ProductDetails from "./pages/ProductDetails";
+
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import Services from "./pages/Services";
 
 const App = () => {
   return (
     <>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/services/:id" element={<ProductDetails />} />
+      </Routes>
+
       <div>App</div>
-      <Form/>
-      <State/>
-      <Parent />
-      <User name="xyz" age={33} skills={["HTML", "CSS", "JS", "JAVA"]} />
     </>
   );
 };
