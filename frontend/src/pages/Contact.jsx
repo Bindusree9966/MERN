d17dd28d8cfac00773202fcfdcc3da393a49ad05
Bindusree9966 <bindusree9966@gmail.com>
2026-03-04 14:20:54 +0530

    react

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import axios from "axios"
+import React, { useEffect, useState } from 'react'
 
 const Contact = () => {
+  const [user,setUsers]=useState([])
+  const fetchData=async()=>{
+    try{
+      const res=await axios.get("https://jsonplaceholder.typicode.com/users/1/posts")
+      setUsers(res.data)
+    }catch(error){
+      console.log('Error:',error)
+    }
+  }
+  useEffect(()=>{
+    fetchData()
+  },[])
   return (
-    <div>Contact</div>
+    <div>
+      <h1>Users</h1>
+      {user.map((user)=>(
+        <div key={user.id}>
+          <h4>Body:{user.body}</h4>
+          <h4>Title:{user.title}</h4>
+        </div>
+      ))}
+    </div>
   )
 }
 
