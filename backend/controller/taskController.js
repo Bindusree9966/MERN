4e4368f95d3998082db54cd151a849a88f18d95f
Bindusree9966 <bindusree9966@gmail.com>
2026-03-10 13:22:31 +0530

    react

--- backend/controller/taskController.js
+++ backend/controller/taskController.js
@@ -1,16 +1,64 @@
-const Task = require("../models/Task");
+const Task = require("../models/task");
 
-// Create a new task    
+// Create task
 exports.createTask = async (req, res) => {
-    try {
-        const task = await Task.create({
-            title:req.body.title,
-            description:req.body.description,
-            status:req.body.status,
-            user:req.user.id
-        })
-        res.status(201).json({msg:'task created'},task);
-    } catch (error) {
-        console.log(error);
+  try {
+    const task = await Task.create({
+      title: req.body.title,
+      description: req.body.description,
+      status: req.body.status,
+      user: req.user.id
+    });
+
+    res.status(201).json({ msg: "task created", task });
+  } catch (error) {
+    res.status(500).json({ msg: "Server error" });
+  }
+};
+
+// Get all tasks
+exports.getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({ user: req.user.id });
+    res.json(tasks);
+  } catch (error) {
+    res.status(500).json({ msg: "Server error" });
+  }
+};
+
+// Get task by ID
+exports.getTaskById = async (req, res) => {
+  try {
+    const task = await Task.findOne({
+      _id: req.params.id,
+      user: req.user.id
+    });
+
+    if (!task) {
+      return res.status(404).json({ msg: "Task not found" });
     }
-}
+
+    res.json(task);
+  } catch (error) {
+    res.status(500).json({ msg: "Server error" });
+  }
+};
+
+// Update task
+exports.updateTask = async (req, res) => {
+  try {
+    const task = await Task.findOneAndUpdate(
+      { _id: req.params.id, user: req.user.id },
+      req.body,
+      { new: true }
+    );
+
+    if (!task) {
+      return res.status(404).json({ msg: "Task not found" });
+    }
+
+    res.json({ msg: "Task updated", task });
+  } catch (error) {
+    res.status(500).json({ msg: "Server error" });
+  }
+};
--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const {createTask}=require('../controller/taskController')
+
+const { createTask, getAllTasks,getTaskById,updateTask } = require("../controller/taskController");
 const {protect}=require('../middleware/authmiddleware')
-router.post('/create',protect,createTask)
+
+router.post("/create", protect,createTask);
+router.get("/getTask", protect,getAllTasks);
+router.get("/getTask/:id", protect,getTaskById);
+router.put("/updateTask/:id",protect,updateTask)
 module.exports = router;
